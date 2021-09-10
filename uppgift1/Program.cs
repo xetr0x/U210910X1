2fe533352e946181fde1f8d7c6768b7a0f877cbf
Kevin Wilusz <kwilusz10@gmail.com>
2021-09-10 13:00:47 +0200

    gjorde en if loop för att skapa en ny användare

--- uppgift1/Program.cs
+++ uppgift1/Program.cs
@@ -6,7 +6,54 @@ namespace uppgift1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            costumer person;
+            person = new costumer();
+
+            Console.WriteLine(" välj (1)Skapa ny kund | (2)Visa antal kunder | (3)Visa lista över alla kunder (4)Avsluta");
+            while (true)
+            {
+
+
+                string x = Console.ReadLine();
+                if (x == "1")
+                {
+                    Console.WriteLine("Mata in ditt namn");
+                    person.name = Console.ReadLine();
+                    Console.WriteLine("Mata in din ålder: ");
+                    person.age = Console.ReadLine();
+                    Console.WriteLine("Mata in din längd: ");
+                    person.height = Console.ReadLine();
+                    Console.WriteLine($"{person.name}  {person.age}  {person.height}");
+                    break;
+                }
+                if (x == "2")
+                {
+
+                }
+
+                if (x == "3")
+                {
+
+                }
+
+                if (x == "4")
+                {
+
+                }
+
+            }
+        }
+
+
+
+
+
+
+        public class costumer
+        {
+            public string name;
+            public string age;
+            public string height;
         }
     }
 }
